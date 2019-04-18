b11ea058e9dcb9231e6fa0db7e0ba5d34c8aa57c
jishaochen <jishaochen@maxrocky.com>
2019-04-18 15:39:40 +0800

    解决乱码问题

--- src/main/java/com/shaochen/swaggerdemo/common/config/SwaggerConfig.java
+++ src/main/java/com/shaochen/swaggerdemo/common/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
